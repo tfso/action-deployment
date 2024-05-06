72b3bd9754d227d985c8f9576887b0bb276c3e26
Øystein Vesth <oystein.vesth@gmail.com>
2024-05-06 14:37:25 +0200

    fix: always set specific URI for beta and prod environments

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,10 @@
 export const getDeploymentUri = (env: string) => {
-    if (process.env.DEPLOYMENT_URI) {
-        return process.env.DEPLOYMENT_URI
+    switch (env.toLowerCase()) {
+        case 'beta':
+            return "https://deployment-beta.api.24sevenoffice.com"
+        case 'prod':
+            return "https://deployment.api.24sevenoffice.com"
+        default:
+            return process.env.DEPLOYMENT_URI || "https://deployment.api.24sevenoffice.com"
     }
-
-    if (env.toLowerCase() === 'beta') {
-        return "https://deployment-beta.api.24sevenoffice.com"
-    }
-    return "https://deployment.api.24sevenoffice.com"
 }
